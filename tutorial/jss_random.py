353b3bf1383fd0f7734b4d1228c3a48d03560b6a
ChristophSchmidl <christoph.schmidl@gmx.net>
2022-08-08 22:06:23 +0200

    Added docs to jss_random.py. Added useful resources in tmp.txt. Extended requirements.txt.

--- tutorial/jss_random.py
+++ tutorial/jss_random.py
@@ -1,29 +1,52 @@
 import gym
 import JSSEnv
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import matplotlib.patches as mpatches
+import pandas as pd
 
-env = gym.make('jss-v1', env_config={'instance_path': './tutorial/instances/taillard/ta41.txt'})
 
-average = 0
+env = gym.make('jss-v1', env_config={'instance_path': './tutorial/instances/taillard/ta01.txt'})
 
-for _ in range(5):
+EPISODES = 200
+
+
+
+episode_rewards = []
+average_episode_rewards = []
+episode_makespans = []
+
+for episode in range(EPISODES):
     print()
     state = env.reset()
     legal_actions = env.get_legal_actions()
     done = False
-    total_reward = 0
+    cum_episode_reward = 0
 
-    machines_available = set()
-    for job in range(len(env.legal_actions[:-1])):
+    #print(env.legal_actions[:-1])
+
+    # The whole purpose here is to get all machine indices into machines_available???
+    machines_available = set() # unordered collection of unique elements
+    for job in range(len(env.legal_actions[:-1])): # -1 to exclude the last action: no-op?
         if env.legal_actions[job]:
-            machine_needed = env.needed_machine_jobs[job]
+            machine_needed = env.needed_machine_jobs[job] # needed_machine_jobs = np.zeros(self.jobs, dtype=np.int)
+            '''
+            env.needed_machine_jobs contains an array of the machines needed for each job.
+            This probably changes every time the step() function is called.
+            '''
             machines_available.add(machine_needed)
 
+    #print(env.needed_machine_jobs)
+
+    # Step through the environment until done
+    steps = 0
     while not done:
+        steps += 1
         actions = np.random.choice(len(legal_actions), 1, p=(legal_actions / legal_actions.sum()))[0]
         state, rewards, done, _ = env.step(actions)
         legal_actions = env.get_legal_actions()
-        total_reward += rewards
+        cum_episode_reward += rewards
 
         machines_available = set()
         for job in range(len(env.legal_actions[:-1])):
@@ -31,10 +54,27 @@ for _ in range(5):
                 machine_needed = env.needed_machine_jobs[job]
                 machines_available.add(machine_needed)
 
+        #print(env.needed_machine_jobs)
+        '''
+        env.needed_machine_jobs contains the indices of the machines needed for each job.
+        If the index contains -1 then the job is not assigned to a machine or the job is assigned to a machine that is not available.
+        [-1 12 -1 -1  1 -1  9 -1 13 -1 -1 11 14 -1  5]
+
+        The last time step looks like this:
+        [-1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1 -1]
+        '''
+
     #env.render().show()
-    print(f"Make-span: {env.last_time_step}")
+    #print(f"Make-span: {env.last_time_step}")
+    episode_rewards.append(cum_episode_reward)
+    episode_makespans.append(env.last_time_step)
+    average_episode_rewards
 
 
-    average += env.last_time_step
-print(f"Total reward: {total_reward}")
-print(f"Average: {average / 100}")
+print(f"Episode rewards: {episode_rewards}")
+print(f"Episode average rewards: {}")
+print(f"Episode make-spans: {episode_makespans}")
+
+data_plot = pd.DataFrame({"Number of Episodes": np.arange(1, len(episode_rewards)+1), "Episode reward":episode_rewards})
+sns.lineplot(x = "Episode", y = "Episode reward", data = data_plot)
+plt.show()
