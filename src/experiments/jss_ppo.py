cce3e7912067d2854550ea00e5d118f8e41d21db
Christoph Schmidl <christoph.schmidl.1@ru.nl>
2022-08-17 13:59:40 +0200

    Updated jss_pp experiment. Created parse_monitor.py to extract episode/rewards

--- src/experiments/jss_ppo.py
+++ src/experiments/jss_ppo.py
@@ -9,11 +9,14 @@ from src.callbacks.StopTrainingOnMaxEpisodes import StopTrainingOnMaxEpisodes
 from src.callbacks.TensorboardCallback import TensorboardCallback
 from stable_baselines3.common import results_plotter
 from stable_baselines3.common.logger import configure
+from stable_baselines3.common.utils import set_random_seed
+from stable_baselines3.common.vec_env import DummyVecEnv
 
 import os
 import time
 import JSSEnv
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import matplotlib.patches as mpatches
@@ -21,7 +24,7 @@ import matplotlib.patches as mpatches
 
 def plot_vanilla(log_folder, min_len):
 
-    x, y = results_plotter.ts2xy(load_results(log_folder), 'timesteps')
+    x, y = results_plotter.ts2xy(load_results(log_folder), results_plotter.X_EPISODES)
 
     x = np.arange(1, len(x) + 1)
     min_len = len(x)
@@ -96,7 +99,28 @@ def plot_reward_per_episode(log_folder, title='Learning Curve'):
     plt.title(title)
     plt.show()
 
+# See: https://github.com/Stable-Baselines-Team/stable-baselines3-contrib/blob/master/docs/modules/ppo_mask.rst
+def mask_fn(env: gym.Env) -> np.ndarray:
+    return env.get_legal_actions()
 
+def make_jobshop_env(rank=0, seed=0, instance_name="taillard/ta01.txt", monitor_log_path=None):
+    """
+    Utility function for multiprocessed env.
+
+    :param seed: (int) the inital seed for RNG
+    :param rank: (int) index of the subprocess
+    """
+    env = gym.make('jss-v1', env_config={'instance_path': f"./data/instances/{instance_name}"})
+    # Important: use a different seed for each environment
+    env.seed(seed + rank)
+    set_random_seed(seed)
+
+    env = ActionMasker(env, mask_fn)
+    # Info on monitor.csv
+    # ep_info = {"r": round(ep_rew, 6), "l": ep_len, "t": round(time.time() - self.t_start, 6)}
+    env = Monitor(env=env, filename=monitor_log_path)
+    return env
+    
 
 ###############################################################
 #                   Create folders and loggers
@@ -115,17 +139,7 @@ new_logger = configure(log_dir, ["stdout", "csv", "tensorboard"])
 #                   Create the environment
 ###############################################################
 
-# See: https://github.com/Stable-Baselines-Team/stable-baselines3-contrib/blob/master/docs/modules/ppo_mask.rst
-def mask_fn(env: gym.Env) -> np.ndarray:
-    return env.get_legal_actions()
-
-# Create the environment
-env = gym.make('jss-v1', env_config={'instance_path': './data/instances/taillard/ta41.txt'})
-env = ActionMasker(env, mask_fn)
-# Info on monitor.csv
-# ep_info = {"r": round(ep_rew, 6), "l": ep_len, "t": round(time.time() - self.t_start, 6)}
-env = Monitor(env=env, filename=log_dir)
-
+env = make_jobshop_env(rank=0, seed=1, instance_name="taillard/ta41.txt", monitor_log_path=log_dir + "monitor.csv")
 # required before you can step through the environment
 env.reset()
 
@@ -155,28 +169,48 @@ model.set_logger(new_logger)
 #                         Train the model
 ###############################################################
 
-
+log_df = pd.DataFrame(columns=['episode', 'timesteps', 'reward'])
 fig = None
 
 TIMESTEPS = np.inf # Dirty hack to make it run forever
-for i in range(1, 2):
+for i in range(0, 3):
+    env = make_jobshop_env(rank=0, seed=i, instance_name="taillard/ta41.txt", monitor_log_path=log_dir + f"episode_{i}")
+    # required before you can step through the environment
+    env.reset()
+
+    model = MaskablePPO('MultiInputPolicy', env, verbose=1, tensorboard_log=log_dir)
+    #model = PPO("MultiInputPolicy", env, verbose=1, tensorboard_log=log_dir)
+    #model.set_logger(new_logger)
+
     # Pass reset_num_timesteps=False to continue the training curve in tensorboard
     # By default, it will create a new curve
     # If you specify different tb_log_name in subsequent runs, you will have split graphs
     # See: https://stable-baselines3.readthedocs.io/en/master/guide/tensorboard.html
-    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=False, tb_log_name="PPO", callback=callback) # TODO: tb_log_name with timestamp, i.e. PPO-{int(time.time())}
+    model.learn(total_timesteps=TIMESTEPS, reset_num_timesteps=True, tb_log_name="PPO", callback=callback, use_masking=True) # TODO: tb_log_name with timestamp, i.e. PPO-{int(time.time())}
     model.save(os.path.join(models_dir, str(TIMESTEPS * i)))
     print(f"Last time step: {env.last_time_step}")
 
     # Grab the monitor.csv file and put it into a pandas dataframe?
     #df = pd.read_csv(os.path.join(log_dir, 'monitor.csv'))
+    episode, episode_reward = results_plotter.ts2xy(load_results(log_dir), results_plotter.X_EPISODES)
+    makespan = env.last_time_step
 
+
+    episode_df = pd.DataFrame(zip(episode, episode_reward), columns=['episode', 'reward'])
+    log_df = pd.concat([log_df, episode_df])
     fig = env.render()
+    print(env.get_episode_rewards())
 
 env.close()
+
+
+log_df.to_csv(log_dir + '/reward_log.csv')
+df = pd.read_csv(log_dir + '/reward_log.csv', index_col=False)
+print(df)
+sns.lineplot(x = "episode", y = "reward", data=df)
+plt.show()
 
 #fig.show()
 #results_plotter.plot_results([log_dir], None, results_plotter.X_EPISODES, "JOB-SHOP")
 #plt.show()
 #plot_vanilla(log_dir)
-
