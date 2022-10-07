2468171287ffc1c2952f4806513aafdfa873d821
Christoph Schmidl <christoph.schmidl.1@ru.nl>
2022-10-07 14:27:36 +0200

    Refactored permutation logic into PermutationHandler class. Tested PermutationHandler with pytest test cases.

---
+++ src/utils/permutation_handler.py
@@ -0,0 +1,50 @@
+from typing import Union, Any, Optional, cast, List
+import numpy as np
+import torch
+
+
+class PermutationHandler:
+    '''
+    This class is used to handle the permutations of different data types
+    '''
+    @staticmethod
+    def permute(data: Union[List, np.ndarray], perm_indices: Optional[list[int]] = None) -> tuple[np.ndarray, torch.Tensor, np.ndarray]:
+        '''
+        Permutes data randomly or based on perm_indices.and the permutation indices (for repeating the same permutation)
+
+        :param data: The data to permute
+        :param perm_indices: The permutation indices. If None, the data will be permuted randomly
+        :return: The permuted data, the permutation matrix (reverse permutation) and the permutation indices (for repeating the same permutation)
+        :rtype: list[Union[List, np.ndarray]]
+        '''
+        data_len = len(data)
+        permuted_data = None
+        perm_matrix = None
+        perm_indices = perm_indices
+
+        if perm_indices is None:
+            # Do random permutation
+            perm_indices = torch.randperm(data_len).long()
+
+        perm_matrix = torch.eye(data_len)[perm_indices].t()
+
+        permuted_data = [ data[i] for i in perm_indices]
+        return np.asarray(permuted_data), perm_matrix, np.asarray(perm_indices)
+
+    def inverse_permute(data: list[Union[List, np.ndarray]], perm_matrix: Optional[list[int]] = None) -> np.ndarray:
+        '''
+        Reverts the already permuted data based on the permutation matrix
+
+        :param data: The data to revert
+        :param perm_matrix: The permutation matrix to revert the data
+        :return: The reverted data
+        :rtype: list[Union[List, np.ndarray]]
+        '''
+        data_len = len(data)
+        reverted_data = None
+
+        restore_indices = torch.Tensor(list(range(data_len))).view(data_len, 1)
+        restore_indices = perm_matrix.mm(restore_indices).view(data_len).long()
+        reverted_data = [ data[i] for i in restore_indices]
+
+        return np.asarray(reverted_data)
---
+++ tests/utils/test_permutation_handler.py
@@ -0,0 +1,39 @@
+import numpy as np
+import torch
+import pytest
+from src.utils.permutation_handler import PermutationHandler
+
+TEST_DATA_LIST = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24], [25, 26, 27, 28, 29, 30], [31, 32, 33, 34, 35, 36]]
+TEST_DATA_NDARRAY = np.array(TEST_DATA_LIST)
+
+
+@pytest.mark.parametrize("data", [TEST_DATA_LIST, TEST_DATA_NDARRAY])
+def test_permute(data):
+    
+    # This is a random permutation
+    random_permuted_data, perm_matrix, perm_indices = PermutationHandler.permute(data)
+
+    assert type(random_permuted_data) is np.ndarray
+    assert type(perm_matrix) is torch.Tensor
+    assert type(perm_indices) is np.ndarray
+
+    # This is a permutation based on perm_indices. The result should be the same as the random permutation.
+    permuted_data, _, _ = PermutationHandler.permute(TEST_DATA_LIST, perm_indices)
+
+    assert np.array_equal(random_permuted_data, permuted_data)
+
+@pytest.mark.parametrize("data", [TEST_DATA_LIST, TEST_DATA_NDARRAY])
+def test_inverse_permute(data):
+    # https://steemit.com/numpy/@luoq/how-to-compute-the-inverse-of-permuation-in-numpy
+    # This is a random permutation
+    random_permuted_data, perm_matrix, perm_indices = PermutationHandler.permute(data)
+
+    assert type(random_permuted_data) is np.ndarray
+    assert type(perm_matrix) is torch.Tensor
+    assert type(perm_indices) is np.ndarray
+
+    # Reverting the random permutation should result in the original data
+    reverted_data = PermutationHandler.inverse_permute(random_permuted_data, perm_matrix)
+    reverted_data = np.asarray(reverted_data)
+
+    assert np.array_equal(reverted_data, TEST_DATA_LIST)
